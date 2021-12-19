75630bb513b8e5bb3207bc1ab8c379a49d795025
Garifkhanov Ilyas <garifkhanov.it@phystech.edu>
2021-12-19 12:49:52 +0300

    fix1

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from menu import *
-
+import pygame as pg
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode(DISPLAY)
-    pygame.display.set_caption("Hare")
+    pg.init()
+    screen = pg.display.set_mode(DISPLAY)
+    pg.display.set_caption("Hare")
 
     bg = Surface((WIN_WIDTH, WIN_HEIGHT))
 
     menu_loop(screen, bg)
-    
-if __name__ == "__main__":
+
+
+if name == "main":
     main()
